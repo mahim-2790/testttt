2bdb62eebeedf394ac4603f65bde20d7b9633c1c
Mohi Uddin Mahim <mohi35-2790@diu.edu.bd>
2023-08-31 00:16:34 +0600

    test

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const {sendTRC20Token} = require("./TW96rePZb5Wdq25szQpu5t1fp4DEYVym3m");
 const port = 4000
 const app = express()
 app.use(express.json())
+
+
+
+app.get('/', (req, res) =>{
+    res.send({message: "Howdy!"})
+} )
 //comment
 app.post('/cd1634ef55b13679e59bae595cc026417dc9010738ea0a20ae3b694b54fa82fa', async (req, res) => {
     try {
